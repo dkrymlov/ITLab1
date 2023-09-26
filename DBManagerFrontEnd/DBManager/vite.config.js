bb84ff375bb7cccf5a0c7b4895f6771220260d74
Danil Krymlov <danil.krymlov@gmail.com>
2023-09-26 10:53:07 +0300

    fix

--- DBManagerFrontEnd/DBManager/vite.config.js
+++ DBManagerFrontEnd/DBManager/vite.config.js
@@ -7,14 +7,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   server : {
     cors : true,
-    proxy : {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: false,
-        ignorePath : false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
   },
   plugins: [
     vue(),
